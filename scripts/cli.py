96fc8055c0188978123d96d81610315848c38273
eharkins <eli.harkins@gmail.com>
2019-11-01 12:02:04 -0700

    comments and help messages

--- scripts/cli.py
+++ scripts/cli.py
@@ -21,10 +21,13 @@ def dry_check_run_cmds(cmds, ctx, outfile=None, shell=False):
 @click.option('--dry/--no-dry', default=False, help='Do not run, just output command args into output file..')
 @click.pass_context
 def cli(ctx, dry):
-    # ensure that ctx.obj exists and is a dict (in case `cli()` is called
-    # by means other than the `if` block below
-    ctx.ensure_object(dict)
-    ctx.obj['DRY'] = dry
+    '''
+    Top level command line interface including top level options such as --dry
+    '''
+    # we explicitly pass --dry to each command (using @click.pass_context) via a click context.
+    # see https://click.palletsprojects.com/en/7.x/complex/ for more on context
+    ctx.ensure_object(dict) # ensure that ctx.obj exists and is a dict (in case `cli()` is called by means other than in the below if __name__ == '__main__': 
+    ctx.obj['DRY'] = dry #set 'DRY' key in the context to the top level option value
 
 @cli.command()
 @click.option('--option1')
@@ -32,6 +35,9 @@ def cli(ctx, dry):
 @click.option('--option3')
 @click.pass_context
 def command1(ctx, option1, option2, option3):
+    '''
+    A command belonging to the top level cli group.
+    '''
     click.echo('Dry run is %s' % (ctx.obj['DRY'] and 'on' or 'off'))
     click.echo('command1 run with option1: {}, option2: {}, option3: {}'.format(option1, option2, option3))
 
